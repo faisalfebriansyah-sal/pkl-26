52694f05f4b78b5fc2f5c3c6632c45b50d572503
Faisal <faisalfebriansyahsaputra9@gmail.com>
2025-12-15 11:52:45 +0700

    tugas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+
+Route::get('/sapa/{nama}', function ($nama) {
+    return "Halo, selamat datang $nama di toko komik kami!";
+});
+
+Route::get('/kategori/{nama?}', function ($nama = 'Semua') {
+    return "Kategori komik: $nama";
+});
+
+Route::get('/produk/{id}', function ($id) {
+    return "Detail produk #$id";
+})->name('produk.detail');
