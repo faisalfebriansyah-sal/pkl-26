53ed8f73cd97fd15a85cc4e1f3aba0699579089d
Faisal <faisalfebriansyahsaputra9@gmail.com>
2025-12-17 10:50:25 +0700

    laravel dan google

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,45 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//contoh
 Route::get('/tentang', function () {
     return view('tentang');
 });
 
+
+//latihan 1
 Route::get('/sapa/{nama}', function ($nama) {
-    return "Halo, selamat datang $nama di toko komik kami!";
+   return "Halo, $nama! Selamat datang di Toko Online.";
 });
 
+
+//latihan 2
 Route::get('/kategori/{nama?}', function ($nama = 'Semua') {
     return "Kategori komik: $nama";
 });
+Auth::routes();
 
-Route::get('/produk/{id}', function ($id) {
-    return "Detail produk #$id";
-})->name('produk.detail');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+
+        // /admin/dashboard
+        Route::get('/dashboard', [AdminController::class, 'dashboard'])
+            ->name('dashboard');
+        // ↑ Nama lengkap route: admin.dashboard
+        // ↑ URL: /admin/dashboard
+
+        // CRUD Produk: /admin/products, /admin/products/create, dll
+        Route::resource('/products', AdminProductController::class);
+        // ↑ resource() membuat 7 route sekaligus:
+        // - GET    /admin/products          → index   (admin.products.index)
+        // - GET    /admin/products/create   → create  (admin.products.create)
+        // - POST   /admin/products          → store   (admin.products.store)
+        // - GET    /admin/products/{id}     → show    (admin.products.show)
+        // - GET    /admin/products/{id}/edit→ edit    (admin.products.edit)
+        // - PUT    /admin/products/{id}     → update  (admin.products.update)
+        // - DELETE /admin/products/{id}     → destroy (admin.products.destroy)
+    });
